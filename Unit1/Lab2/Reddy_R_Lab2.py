2e61bee9bb6021add6a1258796617142a27d9228
Ram Reddy <2023rreddy@tjhsst.edu>
2021-09-23 20:02:02 -0400

    Lab 2 done

--- Unit1/Lab2/Reddy_R_Lab2.py
+++ Unit1/Lab2/Reddy_R_Lab2.py
@@ -2,7 +2,8 @@
 # Date: 09/22/2021
 
 words = set()
-for word in open("words.txt","r"):
+# for word in open("words.txt","r"):
+for word in open("C:\\Users\\kesha\\Desktop\\AI-Course\\Unit1\\Lab2\\words.txt","r"):
     words.add(word[:-1])
 
     
@@ -26,13 +27,8 @@ def display_path(n, explored):  # key: current, value: parent
     while explored[n] != "s":  # "s" is initial's parent
         l.append(n)
         n = explored[n]
-    print()
-    l = l[::-1]
-    for i in l:
-        print(i, end="   ")
-    print()
-    print("\n\nThe shortest path length is :", len(l))
-    return ""
+    l.append(n)
+    return l[::-1]
 
 def BFS(start, end, word_dict):
 
@@ -51,43 +47,59 @@ def BFS(start, end, word_dict):
             explored[a] = s
             
 def recur(start, end, word_dict, explored, limit):
-    new_explored = { key : explored[key] for key in explored }
     if start==end:
         return display_path(start, explored)
     elif limit == 0: return None
     else:
         isCutoff = False
         for a in generate_adjacents(start, word_dict):
-            explored[a] = start
-            result = recur(a, end, word_dict, new_explored, limit-1)
-            if result == None: 
-                isCutoff = True
-            else:
-                return result
+            if a not in explored.keys():
+                explored[a] = start
+                result = recur(a, end, word_dict, { key : explored[key] for key in explored }, limit-1)
+                if result == None: 
+                    isCutoff = True
+                else:
+                    return result
         if isCutoff:
-            return None
-                
-  
-#   Q = []
-#   Q.append(start)
-#   if len(Q) == 0:
-#      return ("No Solution")
-#   s = Q.pop()      
-#   if s == end:
-#      return display_path(s, explored)
-#   for a in generate_adjacents(s, word_dict):
-#      if a not in explored.keys():
-#         Q.append(a)
-        
+            return None        
 
 def DLS(start, end, word_dict, limit):
     explored = {start:"s"}
     return recur(start, end, word_dict, explored, limit-1)
 
-    
+def exitGracefully(start, end, limit=5):
+    if start not in words or len(start) != 6:
+        return 1
+    if end not in words or len(start)!=6:
+        return 1
+    if not isinstance(limit, int):
+        return 1
+    if limit < 1 or limit > 20:
+        return 1;
+    return 0        
+
 def main():
-    print(DLS("foiled","cooper",words,5))
-
-
+    start = input("Type the starting word: ")
+    end = input("Type the goal word: ")
+    if exitGracefully(start, end):
+        return
+    arr = BFS(start,end,words)
+    print(arr)
+    print("The number of steps:", len(arr))
+    limit = int(input("Type the limit (1 - 20): "))
+    start = input("Type the starting word: ")
+    end = input("Type the goal word: ")
+    if exitGracefully(start, end, limit):
+        return
+    arr = DLS(start, end, words, limit)
+    if arr != None:
+        print("Path:", arr)
+        print("steps within",limit ,"limit:", len(arr))
+    else:
+        print("No Soultion")
+    arr = BFS(start, end, words)
+    print("Shortest Path:",arr)
+    print("Steps:",len(arr))
+    
 if __name__ == '__main__':
     main()
